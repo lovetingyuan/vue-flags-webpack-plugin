1d77b9dc10a139e0044543fad3db6e922f157534
Baitu Huang <baitu.huang@shopeemobile.com>
2018-12-29 12:28:04 +0800

    tmp

--- test/vue-cli/vue.config.js
+++ test/vue-cli/vue.config.js
@@ -2,7 +2,7 @@ const Module = require('module')
 const path = require('path')
 const originResolveFilename = Module._resolveFilename
 Module._resolveFilename = function _resolveFilename (request, parent, isMain) {
-  if (/^(webpack|vue-loader|schema-utils|chokidar|postcss-loader)/.test(request)) {
+  if (/^(webpack|vue-loader|schema-utils|chokidar|postcss-loader|vue-template-compiler)/.test(request)) {
     request = path.join(__dirname, 'node_modules', request)
   }
   return originResolveFilename(request, parent, isMain)
@@ -25,10 +25,10 @@ module.exports = {
     plugins: [
       new VueFlagsPlugin({
         flags: path.resolve(__dirname, './flags.js'),
-        watch: false,
-        // files: {
-        //   A: [/HelloWorld/]
-        // }
+        watch: true,
+        files: {
+          A: [/HelloWorld/]
+        }
       })
     ],
   }
